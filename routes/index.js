8b5cdd4dcd33a12e9169a2b38313c2c2beda350a
seong-ho-park <psh30095@gmail.com>
2020-11-08 02:31:11 +0900

    레이아웃 생성

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,10 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET home page. */
-/* GET home page. */
 router.get('/', function (req, res) {
   console.log('/', req.session);
   fs.readFile('views/index.html', function (err, html) {
     res.write(html);
-    res.end(300);
   });
 });
 module.exports = router;
