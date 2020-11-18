27465e54314c7052faf6e2af4e6480e8df629565
seong-ho-park <psh30095@gmail.com>
2020-11-18 13:17:47 +0900

    모바일 페이지 생성

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,11 @@ router.get('/', function (req, res) {
 });
 module.exports = router;
 
-router.get('/', function (req, res) {
+router.get('/m', function (req, res) {
   console.log('/', req.session);
-  fs.readFile('views/index.html', function (err, html) {
-    res.write(html);
+  fs.readFile('views/mindex.html', function (err, html) {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(html);
   });
 });
 module.exports = router;
