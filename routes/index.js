d34b3a6db213b595a91b0f221b1c98f77e09ca08
Cho Seonwoong <zxcvv45@gmail.com>
2020-11-20 06:11:34 +0900

    개인페이지 서버와 연결

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,15 @@ router.get('/JG', function (req, res) {
 });
 module.exports = router;
 
+router.get('/SW', function (req, res) {
+  console.log('/SW', req.session);
+  fs.readFile('views/subpage/Seonwoong.html', function (err, html) {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(html);
+  });
+});
+module.exports = router;
+
 router.get('/login', function (req, res) {
   console.log('/login', req.session);
   fs.readFile('views/login.html', function (err, html) {
